c4b791a3b0be212af606844c09dea83c3c818838
Matthew McCullough <matthewm@ambientideas.com>
2011-05-13 11:19:47 -0500

    Fixing code

--- src/main/java/com/ambientideas/App.java
+++ src/main/java/com/ambientideas/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        ## System.out.println( "Hello World from CI again and again - Changed!" );
+        System.out.println( "Hello World from CI again and again - Changed!" );
     }
 }
